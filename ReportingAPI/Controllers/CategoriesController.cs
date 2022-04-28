ff0e35e4816a6ce7bb1146ee4dcbe22dad7f0d39
vlvshevchuk <vladyslav.shevchuk@arcelormittal.com>
2022-04-28 16:40:53 +0300

    Adding a PUT request

--- ReportingAPI/Controllers/CategoriesController.cs
+++ ReportingAPI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ReportingApi.Dtos;
 using ReportingApi.Models;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
@@ -17,14 +18,16 @@ namespace ReportingApi.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ReportingContext _context;
-        public CategoriesController(ReportingContext context)
+        private IMapper _mapper;
+
+        public CategoriesController(ReportingContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: api/Categories
         [HttpGet]
-
         public async Task<List<Category>> GetCategories()
         {
            // var temp = await _context.Categories.Include(x => x.Reports).ToListAsync();
@@ -33,51 +36,77 @@ namespace ReportingApi.Controllers
             return await _context.Categories.ToListAsync();
         }
 
-        // GET: api/StaticGet
-        /*[HttpGet]
-        public string StaticGet()
+        // PUT: api/PutCategory
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        public async Task<ActionResult> PutCategory(UpdateCategory category)
         {
-            string json = @"
-[
-    {
-        ""id"": 1,
-        ""text"": ""Root node"",
-        ""type"": ""folder"",
-        ""children"": [
+            /*category.ParentId = null;
+            category.Id = 7;
+            category.Text = "test";*/ // Origin: test
+
+            Category Categories = _mapper.Map<Category>(category);
+            _context.Entry(Categories).State = EntityState.Modified;
+
+            try
             {
-                ""id"": 2,
-                ""text"": ""Child node 1"",
-                ""type"": ""file"",
-                ""data"": {
-                    ""url"": ""test data2""
-                }
-            },
-            {
-                ""id"": 3,
-                ""text"": ""Child node 2"",
-                ""type"": ""folder"",
-                ""children"": [
-                    {
-                        ""id"": 6,
-                        ""text"": ""test child"",
-                        ""type"": ""folder""
-                    }
-                ]
-            },
-            {
-                ""id"": 4,
-                ""text"": ""Test node 3"",
-                ""type"": ""file""
+                await _context.SaveChangesAsync();
+                return Ok();
             }
-        ]
-    },
-    {
-        ""id"": 5,
-        ""text"": ""root 2"",
-        ""type"": ""folder""
-    }
-]";
-            return json;
-        }*/
-    }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            //   var tst = Ok();
+           // return Ok();
+        }
+
+            // GET: api/StaticGet
+            /*[HttpGet]
+            public string StaticGet()
+            {
+                string json = @"
+    [
+        {
+            ""id"": 1,
+            ""text"": ""Root node"",
+            ""type"": ""folder"",
+            ""children"": [
+                {
+                    ""id"": 2,
+                    ""text"": ""Child node 1"",
+                    ""type"": ""file"",
+                    ""data"": {
+                        ""url"": ""test data2""
+                    }
+                },
+                {
+                    ""id"": 3,
+                    ""text"": ""Child node 2"",
+                    ""type"": ""folder"",
+                    ""children"": [
+                        {
+                            ""id"": 6,
+                            ""text"": ""test child"",
+                            ""type"": ""folder""
+                        }
+                    ]
+                },
+                {
+                    ""id"": 4,
+                    ""text"": ""Test node 3"",
+                    ""type"": ""file""
+                }
+            ]
+        },
+        {
+            ""id"": 5,
+            ""text"": ""root 2"",
+            ""type"": ""folder""
+        }
+    ]";
+                return json;
+            }*/
+        }
 }
---
+++ ReportingAPI/MappingProfiles/DtoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ReportingApi.Dtos;
+using ReportingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportingApi.MappingProfiles
+{
+    public class DtoMapping : Profile
+    {
+        public DtoMapping()
+        {
+            CreateMap<UpdateCategory, Category>().ReverseMap();
+        }
+    }
+}
