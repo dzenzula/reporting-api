0e6b3ca8e52198198c939c68b4683c73fdcdf706
vlvshevchuk <vladyslav.shevchuk@arcelormittal.com>
2022-09-13 08:05:18 +0300

    Revert "changed Owner type in AddReport.cs"
    This reverts commit c4f7241e4d3d29b9e37fdd34196721aae2a3a20b.

--- ReportingAPI/Controllers/ReportsController.cs
+++ ReportingAPI/Controllers/ReportsController.cs
@@ -196,7 +196,6 @@ namespace ReportingApi.Controllers
         [HttpPost]
         public async Task<ActionResult> PostReport(AddReport reportData)
         {
-            //return Ok();
 			try {
 
                 if (ActiveDirectoryFunction.CheckUserMail(reportData.Owner) is false)
